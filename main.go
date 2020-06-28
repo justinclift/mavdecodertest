bdd4f8c2aa63ea66d411f4c3c909dd37066f77f9
Justin Clift <justin@postgresql.org>
2020-06-28 20:34:22 +1000

    Placeholders added for the message types Ardupilot is broadcasting
    These still need the initial details added to each message type,
    prior to a general refactor and clean up.
    
    Also, the message types here are just the ones Ardupilot seems to
    be broadcasting in a loop.  I haven't yet attempted to add the
    messages it sends in response to a GCS such as QGroundControl.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"syscall"
+	"os"
 	"time"
 
 	"github.com/aler9/gomavlib"
@@ -50,6 +50,65 @@ func main() {
 					fmt.Printf("Heartbeat from drone at %v\n", time.Now().Format(time.RFC1123))
 					fmt.Printf("MAVLink version: %s\n", frm.Frame.GetVersion())
 					fmt.Printf("Drone status: %v\n", msg.SystemStatus)
+
+					fmt.Println()
+
+				case *ardupilotmega.MessageSysStatus: // Id 1
+					fmt.Println("System status message from drone")
+					fmt.Printf("Mainloop time used %d%%, batt voltage: %d, batt current: %d, batt remain: %d\n",
+						msg.Load/10, msg.VoltageBattery, msg.CurrentBattery, msg.BatteryRemaining)
+					fmt.Printf("Comm drop rate: %d, comm errors: %d\n", msg.DropRateComm, msg.ErrorsComm)
+
+					fmt.Println()
+
+				case *ardupilotmega.MessageSystemTime: // Id 2
+					fmt.Println("System time message received from drone")
+					sinceBoot, err := time.ParseDuration(fmt.Sprintf("%dms", msg.TimeBootMs))
+					if err != nil {
+						panic(err)
+					}
+					fmt.Printf("Time since drone boot: %s\n", sinceBoot)
+
+					fmt.Println()
+
+				case *ardupilotmega.MessageParamValue: // Id 22
+					fmt.Printf("Reply message from drone with parameter %s (index %d) value %v\n",
+						msg.ParamId, msg.ParamIndex, msg.ParamValue)
+
+					fmt.Println()
+
+				// TODO: Fill out the details for these
+				case *ardupilotmega.MessageGpsRawInt: // Id 24
+				case *ardupilotmega.MessageRawImu: // Id 27
+				case *ardupilotmega.MessageScaledPressure: // Id 29
+				case *ardupilotmega.MessageAttitude: // Id 30
+				case *ardupilotmega.MessageGlobalPositionInt: // Id 33
+				case *ardupilotmega.MessageRcChannelsRaw: // Id 35
+				case *ardupilotmega.MessageServoOutputRaw: // Id 36
+				case *ardupilotmega.MessageMissionCurrent: // Id 42
+				case *ardupilotmega.MessageNavControllerOutput: // Id 62
+				case *ardupilotmega.MessageRcChannels: // Id 65
+				case *ardupilotmega.MessageVfrHud: // Id 74
+				case *ardupilotmega.MessageScaledImu2: // Id 116
+				case *ardupilotmega.MessagePowerStatus: // Id 125
+				case *ardupilotmega.MessageSensorOffsets: // Id 150
+				case *ardupilotmega.MessageMeminfo: // Id 152
+				case *ardupilotmega.MessageAhrs: // Id 163
+				case *ardupilotmega.MessageHwstatus: // Id 165
+				case *ardupilotmega.MessageAhrs2: // Id 178
+				case *ardupilotmega.MessageAhrs3: // Id 182
+				case *ardupilotmega.MessageEkfStatusReport: // Id 193
+
+				case *ardupilotmega.MessageVibration: // Id 241
+					fmt.Println("Reply message from drone with vibration info")
+					fmt.Printf("Timestamp %v\n", time.Unix(int64(msg.TimeUsec), 0).Format(time.RFC1123))
+					fmt.Printf("Axis vibration levels - x: %.2f y: %.2f z: %.2f\n",
+						msg.VibrationX, msg.VibrationY, msg.VibrationZ)
+					fmt.Printf("Accelerometer clipping counts - 1st: %d 2nd: %d 3rd %d\n",
+						msg.Clipping_0, msg.Clipping_1, msg.Clipping_2)
+
+					fmt.Println()
+
 				default:
 					fmt.Printf("Drone message frame received at: %v\n", time.Now().Format(time.RFC1123))
 
@@ -62,7 +121,10 @@ func main() {
 					fmt.Printf("Message: %+v\n", frm.Message())
 
 					fmt.Printf("Checksum: %v\n", frm.Frame.GetChecksum())
+
+					fmt.Println()
 				}
+				//fmt.Println()
 			case 255:
 				// This is a message from the Ground Control Station
 				switch msg := frm.Message().(type) {
@@ -133,7 +195,7 @@ func main() {
 				// Process a limited amount of event frames from the GCS, then exit
 				i++
 				if i >= 100 {
-					syscall.Exit(0)
+					os.Exit(0)
 				}
 			}
 
